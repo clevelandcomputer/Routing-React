6dd3cda08fdf5a31f4cc4c071a58097e45cd462a
Douglas Wilson <dougwilson0000@yahoo.com>
2020-08-25 14:11:37 -0400

    everything working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as 
+  Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+//Import the Navigation component
+import Navigation from "./components/navigation/Navigation";
+import Page404 from './components/page404/Page404';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* render the Navigation component */}
+        <Navigation />
+        <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="eric" />}
+        />
+         <Route
+          exact
+          path="/welcome"
+          render={(props) => <Welcome {...props} name="eric" />}
+        />
+        
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+
+        
+        <Route
+          exact
+          path="/welcome/:name"
+          render={(props) => <Welcome {...props} name={props.match.params.name} />}
+        />
+        <Route
+          component={Page404}
+        />
+        </Switch>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+//import browser router so we can use it in our application
+import { BrowserRouter } from "react-router-dom";
+//wrap our default App in the BrowserRouter we just imported
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
